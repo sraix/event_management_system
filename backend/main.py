0d39724eb2f930dc679cda1ad05ec210e9bd05f3
sraix <shodhan131@gmail.com>
2024-02-04 12:13:36 +0530

    Initial

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,20 @@
 from flask import Flask,redirect,render_template
- 
+from flask_sqlalchemy import SQLAlchemy
+# mydatabase connection
+local_server=True
+app=Flask(__name__)
+app.secret_key="sraix"
+
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/ems'
+db = SQLAlchemy(app)
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+#testing db connected or not
+@app.route("/test")
+def test():
+    return render_template("index.html")
+app.run(debug=True)
